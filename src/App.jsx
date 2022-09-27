0aa5d86d777428d99ff1b5b61a2698c9326d4bec
Burke Holland <burkeholland@gmail.com>
2022-09-27 18:58:02 +0000

    remove start command

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@
  * To contain application wide settings, routes, state, etc.
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Components/Header";
 
 const App = () => {
-  return <Header/>;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    getMessage();
+  }, [])
+
+  async function getMessage() {
+    const response = await fetch("api/message");
+    const json = await response.json();
+    setMessage(json.message);
+  }
+
+  return (
+    <div>
+      <Header/>
+      <span>{{ message }}</span>
+    </div>
+    )
+
+  ;
 };
 
 export default App;
